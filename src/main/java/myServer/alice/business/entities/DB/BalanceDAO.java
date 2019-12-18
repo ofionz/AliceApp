0191c8de87d2e1ff771e1b049e5385dadfaade09
Ofionz <9802880@gmail.com>
2019-12-18 20:21:10 +0300

    Add docs to balanceDAO and impl

--- src/main/java/myServer/alice/business/entities/DB/BalanceDAO.java
+++ src/main/java/myServer/alice/business/entities/DB/BalanceDAO.java
@@ -3,7 +3,11 @@ package myServer.alice.business.entities.DB;
 import myServer.alice.business.entities.Balance;
 
 public interface BalanceDAO extends DAO {
-
+    /**
+     * Function return current Balance and delete other/older copy of balances
+     * if it exists
+     * @return  {@link Balance}
+     */
     public Balance getBalance();
 
 
